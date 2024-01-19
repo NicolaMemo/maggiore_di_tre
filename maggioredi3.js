4e07bb0affc46280282aed6cfae37b2fafca7ee3
Nicola Memo <n.memo@donboscosandona.it>
2024-01-19 12:40:49 +0100

    dfdvv

--- maggioredi3.js
+++ maggioredi3.js
@@ -1,32 +1,32 @@
-// Definisci una funzione chiamata max_of_three che prende tre parametri: x, y e z
-function max_of_three(x, y, z) {
-  // Inizializza una variabile max_val con il valore 0
-  let max_val = 0;
+// Definisci una funzione chiamata maggiore_dei_tre che prende tre parametri: x, y e z
+function maggiore_dei_tre(x, y, z) {
+  // Inizializza una variabile massimo_valore con il valore 0
+  let massimo_valore = 0;
 
   // Verifica se x è maggiore di y
   if (x > y) {
-    // Se vero, assegna il valore di x a max_val
-    max_val = x;
+    // Se vero, assegna il valore di x a massimo_valore
+    massimo_valore = x;
   } else {
-    // Se falso, assegna il valore di y a max_val
-    max_val = y;
+    // Se falso, assegna il valore di y a massimo_valore
+    massimo_valore = y;
   }
 
-  // Verifica se z è maggiore di max_val
-  if (z > max_val) {
-    // Se vero, aggiorna max_val al valore di z
-    max_val = z;
+  // Verifica se z è maggiore di massimo_valore
+  if (z > massimo_valore) {
+    // Se vero, aggiorna massimo_valore al valore di z
+    massimo_valore = z;
   }
 
-  // Restituisci il valore finale di max_val
-  return max_val;
+  // Restituisci il valore finale di massimo_valore
+  return massimo_valore;
 }
 
-// Stampa il risultato di chiamare max_of_three con gli argomenti 1, 0, 1 sulla console
-console.log("Risultato di max_of_three(1, 0, 1):", max_of_three(1, 0, 1));
+// Stampa il risultato di chiamare maggiore_dei_tre con gli argomenti 1, 0, 1 sulla console
+console.log("Risultato di maggiore_dei_tre(1, 0, 1):", maggiore_dei_tre(1, 0, 1));
 
-// Stampa il risultato di chiamare max_of_three con gli argomenti 0, -10, -20 sulla console
-console.log("Risultato di max_of_three(0, -10, -20):", max_of_three(0, -10, -20));
+// Stampa il risultato di chiamare maggiore_dei_tre con gli argomenti 0, -10, -20 sulla console
+console.log("Risultato di maggiore_dei_tre(0, -10, -20):", maggiore_dei_tre(0, -10, -20));
 
-// Stampa il risultato di chiamare max_of_three con gli argomenti 1000, 510, 440 sulla console
-console.log("Risultato di max_of_three(1000, 510, 440):", max_of_three(1000, 510, 440));
+// Stampa il risultato di chiamare maggiore_dei_tre con gli argomenti 1000, 510, 440 sulla console
+console.log("Risultato di maggiore_dei_tre(1000, 510, 440):", maggiore_dei_tre(1000, 510, 440));
